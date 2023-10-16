f91f7df0f62db52f138c58ae95dd3e6ba6c1f337
Felipe Galdino <57554381+felipeGsousa@users.noreply.github.com>
2023-10-15 23:29:15 -0300

    Creates categories CRUD

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,7 @@ Rails.application.routes.draw do
   resources :orders
   resources :items
   resources :clients
+  resources :categories
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_10_12_024935) do
+ActiveRecord::Schema[7.0].define(version: 2023_10_15_220333) do
+  create_table "categories", force: :cascade do |t|
+    t.string "nome"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
   create_table "clients", force: :cascade do |t|
     t.string "nome"
     t.string "email"
