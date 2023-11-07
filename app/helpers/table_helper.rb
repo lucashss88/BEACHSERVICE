a5e92fbc7c2fe3c140a113b345a72c183adcc475
lucashss88 <lucashss88@gmail.com>
2023-11-07 09:37:34 -0300

    fixing some errors

--- app/helpers/table_helper.rb
+++ app/helpers/table_helper.rb
@@ -1,3 +1,12 @@
 module TableHelper
+  def listTables
+    @clients = Client.all
+    @orders = Order.all
+    render partial: 'shared/tables', locals: { clients: @clients, orders:@orders}
+  end
 
+  def tableNumber
+    @number = rand(1...20)
+    return @number
+  end
 end
