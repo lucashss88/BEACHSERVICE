2522d0259fbe278b18fe2a76163919f19269799b
Felipe Galdino <57554381+felipeGsousa@users.noreply.github.com>
2023-11-06 21:22:21 -0300

    Tables List

--- app/helpers/table_helper.rb
+++ app/helpers/table_helper.rb
@@ -1,3 +1,14 @@
 module TableHelper
 
+  def listTables
+    @clients = Client.all
+    @orders = Order.all
+    render partial: 'shared/tables', locals: { clients: @clients}
+  end
+
+  def tableNumber
+    @number = rand(1...20)
+    return @number
+  end
+
 end
